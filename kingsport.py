c655c04b76178909e6f59660c3497bcaae1845bd
Long Pham <phamlong.th02@gmail.com>
2022-05-29 15:36:24 +0700

    Edit scraper script

--- kingsport.py
+++ kingsport.py
@@ -1,5 +1,6 @@
 import httpx
 import asyncio
+import pandas as pd
 from bs4 import BeautifulSoup
 
 premium = "https://kingsport.vn/ghe-massage/ghe-massage-cao-cap-g-series.html"
@@ -22,26 +23,34 @@ async def get_links(s, url):
 
 	return products
 
-async def get_data(s, url):
+async def get_data(s, url, category):
 	r = await s.get(url, timeout= None)
 	soup = BeautifulSoup(r.text, "lxml")
 	try:
-		data = {
-			"title": soup.select_one("div.product-title h1").text,
-			"price": soup.select_one("div.product-price p.price-1").text,
-			"rating": soup.select_one("span.rating").text,
-		}
-		print(data)
+		title = soup.select_one("div.product-title h1").text
+		price = soup.select_one("div.product-price p.price-1").text
+		rating = soup.select_one("span.rating").text
 	except:
-		data["price"] = soup.select_one("div.product-price p.price-2").text
-		print(data)
-
+		title = soup.select_one("div.product-title h1").text
+		price = soup.select_one("div.product-price p.price-2").text
+		rating = soup.select_one("span.rating").text
+	data = {
+		"title": title,
+		"type": category,
+		"price": price,
+		"rating": rating
+	}
+	return data
 
 async def main():
 	async with httpx.AsyncClient() as s:
 		products = await get_links(s, sitemap)
 
-		tasks = [get_data(s, p[1]) for p in products]
+		tasks = [get_data(s, p[1], p[0]) for p in products]
 		return await asyncio.gather(*tasks)
 
-asyncio.run(main())
+if __name__ == "__main__":
+	results = asyncio.run(main())
+	df = pd.DataFrame(results)
+	df.to_csv("products.csv", index= False)
+	print(df)
